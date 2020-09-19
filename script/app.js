d140cc5ecafad84159816bc8501b3752e7f6b9ee
ms0130k <minseok0130@gmail.com>
2020-09-19 22:09:11 +0900

    2.0 Canvas Events

--- script/app.js
+++ script/app.js
@@ -1,3 +1,28 @@
 const canvas = document.getElementById("jsCanvas");
 
 let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove() {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  console.log(x, y);
+}
+
+function onMouseDown() {
+  painting = true;
+}
+
+function onMouseUp() {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
