07b1605d4cbf018dc0ccb6e73315ff5b91638fce
ms0130k <minseok0130@gmail.com>
2020-09-21 08:15:12 +0900

    2.1 2D context

--- script/app.js
+++ script/app.js
@@ -1,29 +1,39 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove() {
   const x = event.offsetX;
   const y = event.offsetY;
   console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.strokeStyle();
+  }
 }
 
 function onMouseDown() {
   painting = true;
 }
 
-function onMouseUp() {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
