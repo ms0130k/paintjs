e715fdd17428b2a8343caa11bbfe5240bbf22014
ms0130k <minseok0130@gmail.com>
2020-09-22 08:30:50 +0900

    2.2 Recap

--- script/app.js
+++ script/app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
@@ -23,7 +26,7 @@ function onMouseMove() {
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
-    ctx.strokeStyle();
+    ctx.stroke();
   }
 }
 
